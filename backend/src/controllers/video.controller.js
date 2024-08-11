dc026108a418913f8fdfa201d70a187fff20f9df
Priyanshu Modi <priyanshumodi944@gmail.com>
2024-08-12 00:35:56 +0530

    debug getAllVideo

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -7,60 +7,150 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 // debug this function it not run porperly with filteration
-const getAllVideos = asyncHandler(async (req,res) => {
-    const {
-        page = 1,
-        limit = 10,
-        query = '/(^video\/)|(.+)/i',
-        sortBy = 'createdAt',
-        sortType = 1,
-        userId = req.user?._id
-    } = req.query;
-    // TODO: get all Videos based on query, sort, pagination
+// const getAllVideos = asyncHandler(async (req,res) => {
+//     const {
+//         page = 1,
+//         limit = 10,
+//         query = /(^video\/)|(.+)/i,
+//         sortBy = 'createdAt',
+//         sortType = 1,
+//         userId = req.user?._id
+//     } = req.query;
+//     // TODO: get all Videos based on query, sort, pagination
 
-    if(!isValidObjectId(userId)) {
-        throw new ApiError(400, "user doesn't exist");
+//     if(!isValidObjectId(userId)) {
+//         throw new ApiError(400, "user doesn't exist");
+//     }
+
+//     const getAllVideoAggregate = await Video.aggregate([
+//         {
+//             $match: {
+//                 owner: new mongoose.Types.ObjectId(userId),
+//                 $or: [
+//                     {title: {$regex: query, $options: 'i'}},
+//                     {description: {$regex: query, $options: 'i'}}
+//                 ]
+//             }
+//         },
+//         {
+//             $sort: {
+//                 [sortBy]: parseInt(sortType)
+//             }
+//         },
+//         {
+//             $skip: (page -1)*limit,
+//         },
+//         {
+//             $limit: parseInt(limit)
+//         }
+//     ])
+
+//     const videos = await Video.aggregatePaginate(
+//         getAllVideoAggregate,
+//         {
+//             page,
+//             limit
+//         }
+//     )
+
+//     if(!videos) {
+//         throw new ApiError(500, "Something went wrong while fatching videos")
+//     }
+
+//     return res
+//         .status(200)
+//         .json(new ApiResponse(200, videos, "all video fetched successfully"))
+// })
+
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query = "", sortBy = "createdAt", sortType = 1, userId = "" } = req.query
+    //TODO: get all videos based on query, sort, pagination
+    
+    var videoAggregate;
+    try {
+        videoAggregate = await Video.aggregate(
+            [
+                {
+                    $match: {
+                        $or: [
+                            { title: { $regex: query, $options: "i" } },
+                            { description: { $regex: query, $options: "i" } }
+                        ]
+                    }
+
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline: [
+                            {
+                                $project: {
+                                    _id :1,
+                                    fullName: 1,
+                                    avatar: 1,
+                                    username: 1,
+                                }
+                            },
+
+                        ]
+                    }
+                },
+                {
+                    $addFields: {
+                        owner: {
+                            $first: "$owner",
+                        },
+                    },
+                },
+                {
+                    $sort: {
+                        [sortBy || "createdAt"]: sortType || 1
+                    }
+                },
+
+            ]
+        )
+    } catch (error) {
+        // console.error("Error in aggregation:", error);
+        throw new ApiError(500, error.message || "Internal server error in video aggregation");
     }
 
-    const getAllVideoAggregate = await Video.aggregate([
-        {
-            $match: {
-                owner: new mongoose.Types.ObjectId(userId),
-                $or: [
-                    {title: {$regex: query, $options: 'i'}},
-                    {description: {$regex: query, $options: 'i'}}
-                ]
-            }
-        },
-        {
-            $sort: {
-                [sortBy]: parseInt(sortType)
-            }
-        },
-        {
-            $skip: (page -1)*limit,
-        },
-        {
-            $limit: parseInt(limit)
-        }
-    ])
+    const options = {
+        page,
+        limit,
+        customLabels: {
+            totalDocs: "totalVideos",
+            docs: "videos",
 
-    const videos = await Video.aggregatePaginate(
-        getAllVideoAggregate,
-        {
-            page,
-            limit
-        }
-    )
-
-    if(!videos) {
-        throw new ApiError(500, "Something went wrong while fatching videos")
+        },
+        skip: (page - 1) * limit,
+        limit: parseInt(limit),
     }
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, videos, "all video fetched successfully"))
+    Video.aggregatePaginate(videoAggregate, options)
+        .then(result => {
+            // console.log("first")
+            if (result?.videos?.length === 0 ) {
+                return res.status(200).json(new ApiResponse(200, [], "No videos found"))
+            }
+
+            return res.status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        result,
+                        "video fetched successfully"
+                    )
+                )
+        }).catch(error => {
+            // console.log("error ::", error)
+            throw new ApiError(500, error?.message || "Internal server error in video aggregate Paginate")
+        })
 })
+
 
 const publishVideo = asyncHandler(async (req,res) => {
     const {title, description} = req.body
